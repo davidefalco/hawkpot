c123fce8048a36f906feaa3801642fea33aa52d9
Davide Falco <davidefalco@live.it>
2023-12-06 22:11:13 +0100

    Updated manager: now manager can creates compose for multiple installation of plugins/themes in the same Wordpress installation

--- manager.py
+++ manager.py
@@ -1,78 +1,12 @@
 import json
 import yaml
 
-"""
-conf_template = {
-    'version':'3',
-    'services':
-        {'db1':
-                {'image':'mysql:5.7',
-                 'networks':['lan1'], 
-                 'ports':['3306:3306'],
-                 'restart':'always', 
-                 'environment':{'MYSQL_ROOT_PASSWORD':'${MYSQL_ROOT_PASS}',
-                                'MYSQL_DATABASE':'${MYSQL_DB_NAME}', 
-                                'MYSQL_USER':'${MYSQL_USER}',
-                                'MYSQL_PASSWORD':'${MYSQL_PASS}'
-                                }                
-                },
-         'wp1':{
-             'depends_on':['db1'],
-             'image':'wordpress:${WORDPRESS_VERSION}',
-             'networks':['lan1'],
-             'ports':['8001:80'],
-             'working_dir':'/var/www/html',
-             'volumes':['./wp-content1/:/var/www/html/wp-content'],
-             'restart':'always',
-             'environment':{'WORDPRESS_DB_HOST':'db1:3306',
-                            'WORDPRESS_DB_USER':'${WORDPRESS_DB_USER}',
-                            'WORDPRESS_DB_PASSWORD':'${WORDPRESS_DB_PASS}',
-                            'WORDPRESS_DB_NAME':'${MYSQL_DB_NAME}'                 
-                            },
-            },
-         'wpcli1':{
-                'depends_on':['db1', 'wp1'],
-                'image':'wordpress:cli',
-                'networks':['lan1'],
-                'volumes_from':['wp1'],
-                'environment':{'WORDPRESS_DB_HOST':'db1:3306',
-                            'WORDPRESS_DB_USER':'${WORDPRESS_DB_USER}',
-                            'WORDPRESS_DB_PASSWORD':'${WORDPRESS_DB_PASS}',
-                            'WORDPRESS_DB_NAME':'${MYSQL_DB_NAME}'                 
-                            },
-                'command':''' 
-                    /bin/sh -c '
-                    sleep 30;
-                    wp core install --url=localhost:8001 --title=\"${BLOG_TITLE}\" --admin_name=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PSW} --admin_email=${WORDPRESS_MAIL}
-                    '
-                    '''
-                }
-        },
-    'networks':
-        {'lan1':
-            {'ipam':
-                {'driver':'default',
-                 'config':
-                    [{'subnet':'10.0.1.0/24'}]
-                }
-            }
-        }
-}
-"""
-
-#with open('./.template', 'r') as template:
-#    template_yaml = yaml.safe_load(template)
-
-#out = yaml.dump(template_yaml, sort_keys=False)
-#with open('./compose.yml',"w") as compose:
-#    compose.write(out)
-
 # configuration file from user
 with open("./config.json", "r") as conf:
     conf_j = json.loads(conf.read())
 
+sub_ip = conf_j["subnets_ip_address_start"]
 subnets = conf_j["subnets"]
-# the first subnet and nginx system already exists
 
 template_yaml = {
     'version':'3',
@@ -84,15 +18,17 @@ for i in range(1, subnets + 1):
     plg_command = ''
     thm_command = ''
 
-    if conf_j[f'{i}']['plugin']['name'] is not None and conf_j[f'{i}']['plugin']['version'] is not None:
-        plugin_name = conf_j[f'{i}']['plugin']['name']
-        plugin_version = conf_j[f'{i}']['plugin']['version']
-        plg_command = f'wp plugin install {plugin_name} --version={plugin_version} --activate --allow-root'
+    for plugin in conf_j[f'{i}']['plugin']:
+        if plugin['name'] is not None and plugin['version'] is not None:
+            plugin_name = plugin['name']
+            plugin_version = plugin['version']
+            plg_command = ''.join([plg_command, f'wp plugin install {plugin_name} --version={plugin_version} --activate --allow-root\n'])
 
-    if conf_j[f'{i}']['theme']['name'] is not None and conf_j[f'{i}']['theme']['version'] is not None:
-        theme_name = conf_j[f'{i}']['theme']['name']
-        theme_version = conf_j[f'{i}']['theme']['version']
-        thm_command = f'wp theme install {theme_name} --version={theme_version} --activate --allow-root'
+    for theme in conf_j[f'{i}']['theme']:
+        if theme['name'] is not None and theme['version'] is not None:
+            theme_name = theme['name']
+            theme_version = theme['version']
+            thm_command = ''.join([thm_command, f'wp theme install {theme_name} --version={theme_version} --activate --allow-root\n'])
     
 
     wp_service_name = 'wp' + str(i)
@@ -103,7 +39,8 @@ for i in range(1, subnets + 1):
     lan_name = 'lan' + str(i)
     db_port = str(3305 + i)
     port = str(8000 + i)
-    subnet = '10.0.'+str(i)+'.0/24'
+    ip = sub_ip.split(".")
+    subnet = str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + i)+'.'+str(ip[3])+'/24'
 
     update_service_db = {
         db_service_name:{
@@ -180,8 +117,7 @@ for i in range(1, subnets + 1):
 
 out = yaml.dump(template_yaml, sort_keys=False)
 
-with open('./compose_NEW.yml',"w") as compose:
-    compose.write(out)
+print(out)
 
 
     

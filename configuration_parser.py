9544dfaa793040037a71abf93f19174de8e42582
Davide Falco <davidefalco@live.it>
2024-01-18 14:10:49 +0100

    Created configuration_parser module

---
+++ configuration_parser.py
@@ -0,0 +1,152 @@
+import subprocess
+
+def create_rules(subnets : int, ip : list):
+    ipt_rules = '#!/bin/sh\niptables -D DOCKER-USER 1\n'
+    for i in range(1, subnets + 1):
+        # filling the bash file for iptables rules
+        ipt_rules = ''.join([ipt_rules, f'iptables -N COUNT_HP{i}\n', 
+                            f'iptables -A COUNT_HP{i} -m limit --limit 50/day --limit-burst 50 -j ACCEPT\n', 
+                            f'iptables -A COUNT_HP{i} -j LOG --log-prefix \' WARN_HP{i} \'\n',
+                            f'iptables -A COUNT_HP{i} -j REJECT --reject-with icmp-port-unreachable\n',
+                            f'iptables -A DOCKER-USER -s {str(ip[0])+"."+str(ip[1])+"."+str(int(ip[2]) + i)+".5"} ! -d {str(ip[0])+"."+str(ip[1])+"."+str(int(ip[2]) + i)+".10"} -p tcp --dport 443 -j COUNT_HP{i}\n'])
+    return ipt_rules
+
+def save_rules():
+    # saving current docker iptables' rules
+    ipt_command = ['sudo', 'iptables-save']
+    print('Saving current docker iptables\' rules. Plase be sure your docker service is started.')
+    subprocess.run(ipt_command, stdout = open('current-iptables.save', 'w'), check = True)
+
+def make_rev_proxy_service(networks_list : list, ip : list):
+    net_rev_proxy = {}
+    j = 1
+    for net in networks_list:
+        # ip address for each interface of reverse proxy is x.x.x.10
+        inner_net = {
+            f'{net}':{
+                'ipv4_address': str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + j)+'.10'
+            }
+        }
+        net_rev_proxy.update(inner_net)
+        j += 1
+    
+    # reverse proxy services
+    revproxy = {
+        'rev':{
+            'image':'nginx',
+            'ports':['443:443'],
+            'volumes':['./proxy/log:/var/log/nginx/', './proxy/ssl:/etc/nginx/ssl', './proxy/conf:/etc/nginx/conf.d'],
+            'networks': net_rev_proxy
+        }
+    }
+    return revproxy
+
+def make_wp_service(wp_service_name : str, db_service_name : str, lan_name : str, wp_ip : str, port : str, volume_name : str, i : int):
+    update_service_wp = {
+        wp_service_name:{
+            'depends_on':[db_service_name],
+            'image':'wordpress:${WORDPRESS_VERSION}',
+            #'user':'33:33',
+            'networks':{
+                f'{lan_name}':{
+                    'ipv4_address': f'{wp_ip}'
+                }
+            },
+            'ports':[port + str(':80')],
+            'working_dir': f'/var/www/html/hp{i}',
+            'volumes':[volume_name + str(f':/var/www/html/hp{str(i)}/wp-content')],#, f'./site{str(i)}/apache2:/etc/apache2/apache2.conf'],
+            'restart':'always',
+            'environment':{
+                'WORDPRESS_DB_HOST': f'{db_service_name}', #:{str(db_port)}
+                'WORDPRESS_DB_USER':'${WORDPRESS_DB_USER}',
+                'WORDPRESS_DB_PASSWORD':'${WORDPRESS_DB_PASS}',
+                'WORDPRESS_DB_NAME':'${MYSQL_DB_NAME}'                 
+            }
+        }
+    }
+    return update_service_wp
+
+def make_db_service(db_service_name : str, lan_name : str, db_port : str):
+    update_service_db = {
+        db_service_name:{
+            'image':'mysql:5.7',
+            'networks':[lan_name], 
+            'ports':[f'{db_port}:{str(3306)}'],
+            'restart':'always', 
+            'environment':{
+                'MYSQL_ROOT_PASSWORD':'${MYSQL_ROOT_PASS}',
+                'MYSQL_DATABASE':'${MYSQL_DB_NAME}', 
+                'MYSQL_USER':'${MYSQL_USER}',
+                'MYSQL_PASSWORD':'${MYSQL_PASS}'
+            }
+        }
+    }
+    return update_service_db
+
+def make_wpcli_service(wpcli_service_name : str, db_service_name : str, wp_service_name : str, lan_name : str, i : int, dns : str, port : str, title_list : list, plg_command : str, thm_command : str):
+    update_service_wpcli = {
+        wpcli_service_name:{
+            'depends_on':[db_service_name, wp_service_name],
+            'image':'wordpress:cli',
+            'user':'33:33',
+            'networks':[lan_name],
+            'volumes_from':[wp_service_name],
+            'environment':{
+                'WORDPRESS_DB_HOST': f'{db_service_name}', #:{str(db_port)}
+                'WORDPRESS_DB_USER':'${WORDPRESS_DB_USER}',
+                'WORDPRESS_DB_PASSWORD':'${WORDPRESS_DB_PASS}',
+                'WORDPRESS_DB_NAME':'${MYSQL_DB_NAME}'                 
+                },
+            'command':f''' 
+                    /bin/sh -c '
+                    sleep 60;
+                    wp core install --path=/var/www/html/hp{i} --url={dns}:{port}/hp{i} --title=\"{title_list[i - 1]}\" --admin_name=${{WORDPRESS_ADMIN}} --admin_password=${{WORDPRESS_ADMIN_PSW}} --admin_email=${{WORDPRESS_MAIL}}
+                    wp search-replace --path=/var/www/html/hp{i} 'http://{dns}:{port}/hp{i}' 'http://{dns}/hp{i}'
+                    {plg_command}
+                    {thm_command}
+                    '
+                    '''
+        }
+    }
+    return update_service_wpcli
+
+def make_network(lan_name : str, subnet):
+    update_net = {
+        lan_name:
+            {'ipam':
+                {'driver':'default',
+                    'config':
+                        [{'subnet':subnet}]
+                }
+            }
+    }
+    return update_net
+
+def proxy_configuration(dns : str, subnets : int, ip : list):
+    subprocess.call(["mkdir", "-p", "./proxy/conf"])
+    default_conf = 'server{\n\tlisten 443 ssl;\n\tserver_name '+dns+';\n\n\tssl_certificate /etc/nginx/ssl/nginx-selfsigned.crt;\n\tssl_certificate_key /etc/nginx/ssl/nginx-selfsigned.key;\n\n\t'
+    for i in range (1, subnets + 1):
+        # wp_ip is ip address for gateway of each honeypot
+        wp_ip = str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + i)+'.1'
+        default_conf = ''.join([default_conf, f'location /hp{i}'+' {\n\t\t'+'proxy_set_header Host $host;\n\t\tproxy_set_header X-Real-IP $remote_addr;\n\t\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\t\tproxy_set_header X-Forwarded-Proto $scheme;\n\t\tproxy_set_header X-Forwarded-URI $request_uri;\n\t\tproxy_redirect http:// https://;\n\t\t'+f'proxy_pass http://{wp_ip}:{start_port+i}/hp{i};\n\t'+'}'])
+
+    default_conf = ''.join([default_conf, '\n}'])
+    return default_conf
+
+def plugins_command(conf_j : dict, i : int):
+    plg_command = ''
+    for plugin in conf_j[f'{i}']['plugin']:
+        if plugin['name'] is not None and plugin['version'] is not None:
+            plugin_name = plugin['name']
+            plugin_version = plugin['version']
+            plg_command = ''.join([plg_command, f'wp plugin install --path=/var/www/html/hp{i} {plugin_name} --version={plugin_version} --activate --allow-root\n'])
+    return plg_command
+
+def themes_command(conf_j : dict, i : int):
+    thm_command = ''
+    for theme in conf_j[f'{i}']['theme']:
+        if theme['name'] is not None and theme['version'] is not None:
+            theme_name = theme['name']
+            theme_version = theme['version']
+            thm_command = ''.join([thm_command, f'wp theme install --path=/var/www/html/hp{i} {theme_name} --version={theme_version} --activate --allow-root\n'])
+    return thm_command
--- manager.py
+++ manager.py
@@ -4,6 +4,7 @@ import subprocess
 import os
 import time
 import sys
+import configuration_parser as parser
 from threading import Thread
 
 # spostare log a lungo termine
@@ -17,7 +18,7 @@ def log_manager():
 def intrusion_detector():
     subprocess.call(['python3', 'intrusion_detector.py'])
 
-# checking for -s flag
+# checking for -s flag (save to file)
 user_filename = ''
 redirection_flag = False
 if '-s' in sys.argv:
@@ -29,12 +30,10 @@ if '-s' in sys.argv:
         print("Place a name for your file redirection, use: python3 manager.py -s <filename>")
         sys.exit()
 
+# checking for --no-save-rules flag (to not backup old iptables rules)
 user_saveipt_config = False
 if not '--no-save-rules' in sys.argv:
-    # saving current docker iptables' rules
-    ipt_command = ['sudo', 'iptables-save']
-    print('Saving current docker iptables\' rules. Plase be sure your docker service is started.')
-    subprocess.run(ipt_command, stdout = open('current-iptables.save', 'w'), check = True)
+    parser.save_rules()
         
 # configuration file from user
 with open("./config.json", "r") as conf:
@@ -44,178 +43,58 @@ sub_ip = conf_j["subnets_ip_address_start"]
 subnets = conf_j["subnets"]
 start_port = conf_j["start_port"]
 dns = conf_j["dns"]
-
-template_yaml = {
-    'version':'3',
-    'services':{},
-    'networks':{}
-}
-
 ip = sub_ip.split(".")
-ipt_rules = '#!/bin/sh\niptables -D DOCKER-USER 1\n'
+
 networks_list = []
 title_list = []
 for i in range(1, subnets + 1):
     networks_list.append(f'lan{str(i)}')
     title_list.append(conf_j[f"{i}"]["title"])
 
-    # filling the bash file for iptables rules
-    ipt_rules = ''.join([ipt_rules, f'iptables -N COUNT_HP{i}\n', 
-                        f'iptables -A COUNT_HP{i} -m limit --limit 50/day --limit-burst 50 -j ACCEPT\n', 
-                        f'iptables -A COUNT_HP{i} -j LOG --log-prefix \' WARN_HP{i} \'\n',
-                        f'iptables -A COUNT_HP{i} -j REJECT --reject-with icmp-port-unreachable\n',
-                        f'iptables -A DOCKER-USER -s {str(ip[0])+"."+str(ip[1])+"."+str(int(ip[2]) + i)+".5"} ! -d {str(ip[0])+"."+str(ip[1])+"."+str(int(ip[2]) + i)+".10"} -p tcp --dport 443 -j COUNT_HP{i}\n'])
 
+ipt_rules = parser.create_rules(subnets, ip)
 with open('rules.sh', 'w') as file:
     file.write(ipt_rules)
 
 #sys.exit()
-
-net_rev_proxy = {}
-j = 1
-for net in networks_list:
-    # ip address for each interface of reverse proxy is x.x.x.10
-    inner_net = {
-        f'{net}':{
-            'ipv4_address': str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + j)+'.10'
-        }
-    }
-    net_rev_proxy.update(inner_net)
-    j += 1
-
-'''
-networks = {}
-networks.update(net_rev_proxy)
-print(networks)
-sys.exit()
-'''
-
-# reverse proxy service
-revproxy = {
-    'rev':{
-        'image':'nginx',
-        'ports':['443:443'],
-        'volumes':['./proxy/log:/var/log/nginx/', './proxy/ssl:/etc/nginx/ssl', './proxy/conf:/etc/nginx/conf.d'],
-        'networks': net_rev_proxy
-    }
+    
+template_yaml = {
+    'version':'3',
+    'services':{},
+    'networks':{}
 }
 
+# make reverse proxy service
+revproxy = parser.make_rev_proxy_service(networks_list, ip)
 template_yaml['services'].update(revproxy)
-
 
 # default.conf configuration for the proxy
 if not os.path.exists("./proxy/conf/default.conf"):
-    subprocess.call(["mkdir", "-p", "./proxy/conf"])
-    default_conf = 'server{\n\tlisten 443 ssl;\n\tserver_name '+dns+';\n\n\tssl_certificate /etc/nginx/ssl/nginx-selfsigned.crt;\n\tssl_certificate_key /etc/nginx/ssl/nginx-selfsigned.key;\n\n\t'
-    for i in range (1, subnets + 1):
-        # wp_ip is ip address for gateway of each honeypot
-        wp_ip = str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + i)+'.1'
-        default_conf = ''.join([default_conf, f'location /hp{i}'+' {\n\t\t'+'proxy_set_header Host $host;\n\t\tproxy_set_header X-Real-IP $remote_addr;\n\t\tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n\t\tproxy_set_header X-Forwarded-Proto $scheme;\n\t\tproxy_set_header X-Forwarded-URI $request_uri;\n\t\tproxy_redirect http:// https://;\n\t\t'+f'proxy_pass http://{wp_ip}:{start_port+i}/hp{i};\n\t'+'}'])
-
-    default_conf = ''.join([default_conf, '\n}'])
+    default_conf = parser.proxy_configuration(dns, subnets, ip)
     with open('./proxy/conf/default.conf', 'w') as proxyconf:
         proxyconf.write(default_conf)   
 
+# make wp, mysql, wpcli services
 for i in range(1, subnets + 1):
-    plg_command = ''
-    thm_command = ''
+    plg_command = parser.plugins_command(conf_j, i)
+    thm_command = parser.themes_command(conf_j, i)
 
-    for plugin in conf_j[f'{i}']['plugin']:
-        if plugin['name'] is not None and plugin['version'] is not None:
-            plugin_name = plugin['name']
-            plugin_version = plugin['version']
-            plg_command = ''.join([plg_command, f'wp plugin install --path=/var/www/html/hp{i} {plugin_name} --version={plugin_version} --activate --allow-root\n'])
+    wp_service_name = 'wp' + str(i)                         # name for wp services
+    db_service_name = 'db' + str(i)                         # name for db services
+    wpcli_service_name = 'wpcli' + str(i)                   # name for wpcli services
+    volume_name = f'./hp{str(i)}/wp-content' + str(i) + '/' # volume for wp services
+    lan_name = 'lan' + str(i)                               # name for lans
+    port = str(start_port + i)                              # port for wp services
+    db_port = str(3305 + i)                                 # port for db services
 
-    for theme in conf_j[f'{i}']['theme']:
-        if theme['name'] is not None and theme['version'] is not None:
-            theme_name = theme['name']
-            theme_version = theme['version']
-            thm_command = ''.join([thm_command, f'wp theme install --path=/var/www/html/hp{i} {theme_name} --version={theme_version} --activate --allow-root\n'])
-    
-    wp_service_name = 'wp' + str(i)
-    db_service_name = 'db' + str(i)
-    volume_name = f'./hp{str(i)}/wp-content' + str(i) + '/'
-    wpcli_service_name = 'wpcli' + str(i)
-
-    lan_name = 'lan' + str(i)
-    db_port = str(3305 + i)
-    port = str(start_port + i)
     ip = sub_ip.split(".")
     subnet = str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + i)+'.'+str(ip[3])+'/24'
-    wp_ip = str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + i)+'.5' # fixed ip address for all wp services
+    wp_ip = str(ip[0])+'.'+str(ip[1])+'.'+str(int(ip[2]) + i)+'.5' # last byte of each ip address for each wp service is 5
 
-    update_service_db = {
-        db_service_name:{
-            'image':'mysql:5.7',
-            'networks':[lan_name], 
-            'ports':[f'{db_port}:{str(3306)}'],
-            'restart':'always', 
-            'environment':{
-                'MYSQL_ROOT_PASSWORD':'${MYSQL_ROOT_PASS}',
-                'MYSQL_DATABASE':'${MYSQL_DB_NAME}', 
-                'MYSQL_USER':'${MYSQL_USER}',
-                'MYSQL_PASSWORD':'${MYSQL_PASS}'
-            }
-        }
-    }
-
-    update_service_wp = {
-        wp_service_name:{
-            'depends_on':[db_service_name],
-            'image':'wordpress:${WORDPRESS_VERSION}',
-#            'user':'33:33',
-            'networks':{
-                f'{lan_name}':{
-                    'ipv4_address': f'{wp_ip}'
-                }
-            },
-            'ports':[port + str(':80')],
-            'working_dir': f'/var/www/html/hp{i}',
-            'volumes':[volume_name + str(f':/var/www/html/hp{str(i)}/wp-content')],#, f'./site{str(i)}/apache2:/etc/apache2/apache2.conf'],
-            'restart':'always',
-            'environment':{
-                'WORDPRESS_DB_HOST': f'{db_service_name}', #:{str(db_port)}
-                'WORDPRESS_DB_USER':'${WORDPRESS_DB_USER}',
-                'WORDPRESS_DB_PASSWORD':'${WORDPRESS_DB_PASS}',
-                'WORDPRESS_DB_NAME':'${MYSQL_DB_NAME}'                 
-            }
-        }
-    }
-
-    update_service_wpcli = {
-        wpcli_service_name:{
-            'depends_on':[db_service_name, wp_service_name],
-            'image':'wordpress:cli',
-            'user':'33:33',
-            'networks':[lan_name],
-            'volumes_from':[wp_service_name],
-            'environment':{
-                'WORDPRESS_DB_HOST': f'{db_service_name}', #:{str(db_port)}
-                'WORDPRESS_DB_USER':'${WORDPRESS_DB_USER}',
-                'WORDPRESS_DB_PASSWORD':'${WORDPRESS_DB_PASS}',
-                'WORDPRESS_DB_NAME':'${MYSQL_DB_NAME}'                 
-                },
-            'command':f''' 
-                    /bin/sh -c '
-                    sleep 60;
-                    wp core install --path=/var/www/html/hp{i} --url={dns}:{port}/hp{i} --title=\"{title_list[i - 1]}\" --admin_name=${{WORDPRESS_ADMIN}} --admin_password=${{WORDPRESS_ADMIN_PSW}} --admin_email=${{WORDPRESS_MAIL}}
-                    wp search-replace --path=/var/www/html/hp{i} 'http://{dns}:{port}/hp{i}' 'http://{dns}/hp{i}'
-                    {plg_command}
-                    {thm_command}
-                    '
-                    '''
-        }
-    }
-
-    update_net = {
-        lan_name:
-            {'ipam':
-                {'driver':'default',
-                    'config':
-                        [{'subnet':subnet}]
-                }
-            }
-    }
+    update_service_wp = parser.make_wp_service(wp_service_name, db_service_name, lan_name, wp_ip, port, volume_name, i)
+    update_service_db = parser.make_db_service(wp_service_name, lan_name, db_port)
+    update_service_wpcli = parser.make_wpcli_service(wpcli_service_name, db_service_name, wp_service_name, lan_name, i, dns, port, title_list, plg_command, thm_command)
+    update_net = parser.make_network(lan_name, subnet)
 
     template_yaml['services'].update(update_service_db)
     template_yaml['services'].update(update_service_wp)
@@ -224,14 +103,12 @@ for i in range(1, subnets + 1):
 
 out = yaml.dump(template_yaml, sort_keys=False)
 
-
 if redirection_flag:
     with open(user_filename, 'w') as f:
         f.write(out)
 else: 
     if '--no-verbose' not in sys.argv:
         print(out)
-
 
 if '--no-apply-rules' not in sys.argv:
     subprocess.call(['sudo', 'sh', './rules.sh'])
@@ -249,6 +126,3 @@ try:
 except KeyboardInterrupt:
     print('Exiting...')
     sys.exit()
-
-
-

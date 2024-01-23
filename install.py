f9219f24539426c817dc81370f0349269d85bd65
hawk <davidefalco@live.it>
2024-01-23 12:15:03 +0100

    Created installer.

---
+++ install.py
@@ -0,0 +1,22 @@
+import os
+
+dir = os.getcwd()
+
+service = f'''[Unit]
+Description=My test service
+After=multi-user.target
+
+[Service]
+Type=simple
+Restart=no
+User=root
+ExecStart=/usr/bin/python3 {dir}/hp_manager.py --no-apply-rules
+
+StandardOutput=null
+
+[Install]
+WantedBy=multi-user.target
+'''
+
+with open('/etc/systemd/system/hawkpot.service', 'w') as service_file:
+    service_file.write(service)
